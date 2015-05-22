6159cc046bd62b2a51ca75919958492dc8562f32
Walsh Costigan <walshcostigan@gmail.com>
2015-05-21 21:03:56 -0400

    Created gender guesser from API

---
+++ homework_starter_code/Ruby/Ruby_Midterm/gender.rb
@@ -0,0 +1,21 @@
+puts "Hello, welcome to your new favorite game. Tell me your name, and I will guess your gender!"
+
+require 'unirest'
+require "JSON"
+
+def guess_gender
+
+	print "What's your name?"
+	name = gets.chomp
+
+	result = Unirest.get "https://montanaflynn-gender-guesser.p.mashape.com/?name=#{name}",
+	  headers:{
+	    "X-Mashape-Key" => "WJQWvWJCdxmshaVi9mIDPwuxMtACp1dcCPRjsnHjjpsqBO7Atw",
+	    "Accept" => "application/json"
+	  }
+
+  	puts result.body["gender"]
+
+end
+
+guess_gender
