7f4372a61cf2bf557a53e7c43e27e108f5991bd2
Walsh Costigan <walshcostigan@gmail.com>
2015-06-08 18:49:37 -0400

    Created seamlessdocs forms viewer

---
+++ FormSearch/app/models/form.rb
@@ -0,0 +1,8 @@
+class Form < ActiveRecord::Base
+
+	STATE = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa","Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+
+	DEPARTMENT = ["Administration", "Animal Shelter", "Building/Zoning", "Clerk", "Emergency Management", "Environmental Health", "Finance", "Health Department", "Human Resources", "Information Technology", "Library", "Parks and Rec", "Public Works", "Purchasing", "Special Events", "Tax Department", "Utilities" ]
+
+end
+
---
+++ FormSearch/app/views/forms/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @form, :id, :form_name, :form_link, :municipality_name, :municipality_state, :form_department, :created_at, :updated_at
